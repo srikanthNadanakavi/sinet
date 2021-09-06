4cf445bfdbb945020ef539332b92e6a966aef889
Srikanth Nadanakavi <Srikanth.Nadanakavi@mvc.nj.gov>
2021-09-06 15:25:14 -0400

    Angulat basic setup

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,10 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { IPagination } from './models/IPagination';
+import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Client';
+  products: IProduct[];
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http
+      .get('https://localhost:5001/api/products?pageSize=50')
+      .subscribe((response: IPagination) => {
+        // console.l;
+        console.log(response.data);
+        this.products = response.data;
+      });
+  }
 }
