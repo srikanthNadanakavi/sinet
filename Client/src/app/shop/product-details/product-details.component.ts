fc8d66ddcd717fab18ef737cbdb4c90c51218f3b
Srikanth Nadanakavi <Srikanth.Nadanakavi@mvc.nj.gov>
2021-09-12 13:25:22 -0400

    implemeted app routing and lazy loading

---
+++ Client/src/app/shop/product-details/product-details.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduct } from 'src/app/shared/models/product';
+import { ShopService } from '../shop.service';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css'],
+})
+export class ProductDetailsComponent implements OnInit {
+  product: IProduct;
+  constructor(
+    private shopService: ShopService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const id = +this.activatedRoute.snapshot.paramMap.get('id');
+    this.shopService.getProduct(id).subscribe(
+      (product) => {
+        this.product = product;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+}
