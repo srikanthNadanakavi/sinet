cda23dbb99fad1d30b7eb6a8cd1e5f2eb963c5e1
Srikanth Nadanakavi <Srikanth.Nadanakavi@mvc.nj.gov>
2021-09-14 15:01:09 -0400

    client error handling

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotfoundErrorComponent } from './core/notfound-error/notfound-error.component';
+import { ServerErrorComponent } from './core/server-error/server-error.component';
+import { TestErrorComponent } from './core/test-error/test-error.component';
 import { HomeComponent } from './home/home.component';
-import { ProductDetailsComponent } from './shop/product-details/product-details.component';
-import { ShopComponent } from './shop/shop.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'server-error', component: ServerErrorComponent },
+  { path: 'not-found', component: NotfoundErrorComponent },
+  { path: 'test-errors', component: TestErrorComponent },
   {
     path: 'shop',
     loadChildren: () =>
--- Client/src/app/core/core.module.ts
+++ Client/src/app/core/core.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
+import { TestErrorComponent } from './test-error/test-error.component';
+import { ToastrModule } from 'ngx-toastr';
+import { NotfoundErrorComponent } from './notfound-error/notfound-error.component';
+import { ServerErrorComponent } from './server-error/server-error.component';
 
 @NgModule({
-  declarations: [NavBarComponent],
-  imports: [CommonModule, RouterModule],
+  declarations: [
+    NavBarComponent,
+    TestErrorComponent,
+    NotfoundErrorComponent,
+    ServerErrorComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+    }),
+  ],
   exports: [NavBarComponent],
 })
 export class CoreModule {}
